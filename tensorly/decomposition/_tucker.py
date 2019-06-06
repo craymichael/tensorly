ec0e9da62ce3265346877db95ed5fe4b9b0dd5b3
Zach Carmichael <zjc2920@g.rit.edu>
2019-06-05 21:17:04 -0400

    Added support for tf_graph + a few minor doc fixes

--- tensorly/decomposition/_tucker.py
+++ tensorly/decomposition/_tucker.py
@@ -3,13 +3,13 @@ from ..base import unfold
 from ..tenalg import multi_mode_dot, mode_dot
 from ..tucker_tensor import tucker_to_tensor
 from ..random import check_random_state
-from math import sqrt
-
+import sys
 import warnings
 
 # Author: Jean Kossaifi <jean.kossaifi+tensors@gmail.com>
 
 # License: BSD 3 clause
+
 
 def partial_tucker(tensor, modes, rank=None, n_iter_max=100, init='svd', tol=10e-5,
                    svd='numpy_svd', random_state=None, verbose=False, ranks=None):
@@ -22,7 +22,7 @@ def partial_tucker(tensor, modes, rank=None, n_iter_max=100, init='svd', tol=10e
     tensor : ndarray
     modes : int list
             list of the modes on which to perform the decomposition
-    ranks : None or int list
+    rank : None or int list
             size of the core tensor, ``(len(ranks) == len(modes))``
     n_iter_max : int
                  maximum number of iteration
@@ -47,7 +47,9 @@ def partial_tucker(tensor, modes, rank=None, n_iter_max=100, init='svd', tol=10e
 
     """
     if ranks is not None:
-        message = "'ranks' is depreciated, please use 'rank' instead"
+        if rank is not None:
+            raise ValueError("Cannot specify both 'rank' and deprecated 'ranks' args")
+        message = "'ranks' is deprecated, please use 'rank' instead"
         warnings.warn(message, DeprecationWarning)
         rank = ranks
 
@@ -56,7 +58,8 @@ def partial_tucker(tensor, modes, rank=None, n_iter_max=100, init='svd', tol=10e
         warnings.warn(message, Warning)
         rank = [tl.shape(tensor)[mode] for mode in modes]
     elif isinstance(rank, int):
-        message = "Given only one int for 'rank' intead of a list of {} modes. Using this rank for all modes.".format(len(modes))
+        message = "Given only one int for 'rank' intead of a list of {} modes. " \
+                  "Using this rank for all modes.".format(len(modes))
         warnings.warn(message, Warning)
         rank = [rank for _ in modes]
 
@@ -76,32 +79,99 @@ def partial_tucker(tensor, modes, rank=None, n_iter_max=100, init='svd', tol=10e
     else:
         rng = check_random_state(random_state)
         core = tl.tensor(rng.random_sample(rank), **tl.context(tensor))
-        factors = [tl.tensor(rng.random_sample((tl.shape(tensor)[mode], rank[index])), **tl.context(tensor)) for (index, mode) in enumerate(modes)]
+        factors = [tl.tensor(rng.random_sample((tl.shape(tensor)[mode], rank[index])), **tl.context(tensor))
+                   for (index, mode) in enumerate(modes)]
 
-    rec_errors = []
     norm_tensor = tl.norm(tensor, 2)
 
-    for iteration in range(n_iter_max):
-        for index, mode in enumerate(modes):
-            core_approximation = multi_mode_dot(tensor, factors, modes=modes, skip=index, transpose=True)
-            eigenvecs, _, _ = svd_fun(unfold(core_approximation, mode), n_eigenvecs=rank[index])
-            factors[index] = eigenvecs
+    if tl.get_backend() == 'tensorflow_graph':
+        import tensorflow as tf
+        import numpy as np
 
-        core = multi_mode_dot(tensor, factors, modes=modes, transpose=True)
-
-        # The factors are orthonormal and therefore do not affect the reconstructed tensor's norm
-        rec_error = sqrt(abs(norm_tensor**2 - tl.norm(core, 2)**2)) / norm_tensor
-        rec_errors.append(rec_error)
-
-        if iteration > 1:
+        def cond(iteration, core, rec_error_prev, rec_error_curr, *factors):  # The condition to exit while loop
+            condition = tf.less(iteration, n_iter_max)  # Max iters reached
+            variation = rec_error_prev - rec_error_curr
             if verbose:
-                print('reconsturction error={}, variation={}.'.format(
-                    rec_errors[-1], rec_errors[-2] - rec_errors[-1]))
+                print_op = tf.cond(
+                    tf.greater(iteration, 1),
+                    true_fn=lambda: tf.print('reconstruction error=', rec_error_curr, ', variation=',
+                                             variation, output_stream=sys.stdout),
+                    false_fn=lambda: tf.constant(True)
+                )
+                with tf.control_dependencies([print_op]):
+                    variation = tf.identity(variation)
+            if tol:
+                def tol_check():
+                    above_tol = tf.greater(tl.abs(variation), tol)
+                    # Print convergence iterations
+                    if verbose:
+                        print_op = tf.cond(
+                            above_tol, true_fn=lambda: tf.constant(True),
+                            false_fn=lambda: tf.print('converged in', iteration,
+                                                      'iterations.',
+                                                      output_stream=sys.stdout)
+                        )
+                        with tf.control_dependencies([print_op]):
+                            above_tol = tf.identity(above_tol)
+                    return tf.logical_and(condition, above_tol)
+                condition = tf.cond(tf.greater(iteration, 1),
+                                    true_fn=tol_check,
+                                    false_fn=lambda: condition)
+            return condition
 
-            if tol and abs(rec_errors[-2] - rec_errors[-1]) < tol:
+        def body(iteration, core, rec_error_prev, rec_error_curr, *factors):  # While loop body
+            new_factors = []
+            for index, mode in enumerate(modes):
+                core_approximation = multi_mode_dot(tensor, factors, modes=modes,
+                                                    skip=index, transpose=True)
+                eigenvecs, _, _ = svd_fun(unfold(core_approximation, mode),
+                                          n_eigenvecs=rank[index])
+                new_factors.append(eigenvecs)
+            core = multi_mode_dot(tensor, new_factors, modes=modes, transpose=True)
+            # The factors are orthonormal and therefore do not affect the
+            # reconstructed tensor's norm
+            rec_error_prev = rec_error_curr
+            rec_error_curr = tl.sqrt(tl.abs(norm_tensor ** 2 - tl.norm(core, 2) ** 2)) / norm_tensor
+            return [tf.add(iteration, 1), core, rec_error_prev, rec_error_curr, *new_factors]
+
+        dtype = tl.get_dtype(tensor)
+        dummy_shape = list(tl.shape(tensor))
+        for mode, rank_i in zip(modes, rank):
+            dummy_shape[mode] = rank_i
+        dummy_core = tf.constant(np.empty(dummy_shape, dtype=dtype))
+        dummy_err_prev = tf.constant(0., dtype=dtype)
+        dummy_err_curr = tf.constant(float('+inf'), dtype=dtype)
+        _, core, _, _, *factors = tf.while_loop(
+            cond, body, back_prop=False, parallel_iterations=1,
+            loop_vars=[tf.constant(0), dummy_core, dummy_err_prev,
+                       dummy_err_curr, *factors]
+        )
+        return core, factors
+
+    else:
+        rec_errors = []
+
+        for iteration in range(n_iter_max):
+            for index, mode in enumerate(modes):
+                core_approximation = multi_mode_dot(tensor, factors, modes=modes, skip=index, transpose=True)
+                eigenvecs, _, _ = svd_fun(unfold(core_approximation, mode), n_eigenvecs=rank[index])
+                factors[index] = eigenvecs
+
+            core = multi_mode_dot(tensor, factors, modes=modes, transpose=True)
+
+            # The factors are orthonormal and therefore do not affect the reconstructed tensor's norm
+            rec_error = tl.sqrt(tl.abs(norm_tensor**2 - tl.norm(core, 2)**2)) / norm_tensor
+            rec_errors.append(rec_error)
+
+            if iteration > 1:
                 if verbose:
-                    print('converged in {} iterations.'.format(iteration))
-                break
+                    print('reconstruction error={}, variation={}.'.format(
+                        rec_errors[-1], rec_errors[-2] - rec_errors[-1]))
+
+                if tol and abs(rec_errors[-2] - rec_errors[-1]) < tol:
+                    if verbose:
+                        print('converged in {} iterations.'.format(iteration))
+                    break
 
     return core, factors
 
@@ -149,6 +219,7 @@ def tucker(tensor, rank=None, ranks=None, n_iter_max=100, init='svd',
                           svd=svd, tol=tol, random_state=random_state, verbose=verbose)
 
 
+# TODO: add `svd` argument to pass through to `tucker()`
 def non_negative_tucker(tensor, rank, n_iter_max=10, init='svd', tol=10e-5,
                         random_state=None, verbose=False, ranks=None):
     """Non-negative Tucker decomposition
@@ -194,7 +265,6 @@ def non_negative_tucker(tensor, rank, n_iter_max=10, init='svd', tol=10e-5,
         message = "Given only one int for 'rank' for decomposition a tensor of order {}. Using this rank for all modes.".format(n_mode)
         warnings.warn(message, RuntimeWarning)
         rank = [rank]*n_mode
-
 
     epsilon = 10e-12
 
